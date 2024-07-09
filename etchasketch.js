f1de905b9c8adedc18b59ab08e2415d88beca3df
cooipie4 <binh071006@gmail.com>
2024-07-09 16:13:25 +0700

    Added a trail of black squares when hovering

--- etchasketch.js
+++ etchasketch.js
@@ -1,18 +1,32 @@
 function populateBoard(size){
   let board = document.getElementById('board'); // Get the element by ID
-  let squares = board.querySelectorAll('div');
-  squares.forEach(div => div.remove());
+  board.innerHTML='';
 board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
 board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 let amount = size*size
 for (let i = 0; i < amount; i++) {
   let square = document.createElement('div');
-  square.style.backgroundColor = 'grey'; // Add quotes around the color
+  square.classList.add('square');
+  square.style.backgroundColor = 'white'; // Add quotes around the color
   board.appendChild(square); // Use appendChild for simplicity
 }
+board.addEventListener('mouseover', (event) => {
+  if (event.target.classList.contains('square')) {
+    event.target.style.backgroundColor = 'black';
+  }
+});
+
+board.addEventListener('mouseout', (event) => {
+  if (event.target.classList.contains('square')) {
+    event.target.style.backgroundColor = 'grey';
+  }
+});
 }
 populateBoard(16);
 
+
 function changeSize(input){
+  parseInt(input);
   populateBoard(input);
 }
+
